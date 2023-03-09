226d845a7b12d0919fba4733a0b462aaa280881a
yaya <yaya@xyyaya.com>
2023-03-09 23:58:44 +0800

    feat: 修正index.ts的代码规范化问题

--- src/index.ts
+++ src/index.ts
@@ -13,57 +13,57 @@ export interface IStartupOptions {
   arguments?: string[];
 }
 
-export async function container(options: IStartupOptions) {
+export const container = async (options: IStartupOptions) => {
   const config = await loadConfig(options);
   await Application.startLog(config.debug, config.logger).catch((err: ExError) => {
-    // tslint:disable-next-line
+    // eslint-disable-next-line no-console
     console.log(`${err.name}: ${err.message}`);
     process.exit(1);
   });
   await Application.startContainer(config).catch(async (err: ExError) => {
-    Application.appLog.fatal('start', err, { error: Logger.errorMessage(err) });
+    Application.appLog.fatal('start', err, {error: Logger.errorMessage(err)});
     await Runtime.shutdown();
     process.exit(1);
   });
-}
+};
 
-export async function server(options: IStartupOptions) {
+export const server = async (options: IStartupOptions) => {
   const config = await loadConfig(options);
   await Application.startLog(config.debug, config.logger).catch((err: ExError) => {
-    // tslint:disable-next-line
+    // eslint-disable-next-line no-console
     console.log(`${err.name}: ${err.message}`);
     process.exit(1);
   });
   await Application.startServer(config).catch(async (err: ExError) => {
-    Application.appLog.fatal('start', err, { error: Logger.errorMessage(err) });
+    Application.appLog.fatal('start', err, {error: Logger.errorMessage(err)});
     await Runtime.shutdown();
     process.exit(1);
   });
-}
+};
 
-export async function command(options: IStartupOptions) {
+export const command = async (options: IStartupOptions) => {
   const config = await loadConfig(options);
   await Application.startOnlyAppLog(config.debug, config.logger).catch((err: ExError) => {
-    // tslint:disable-next-line
+    // eslint-disable-next-line no-console
     console.log(`${err.name}: ${err.message}`);
     process.exit(1);
   });
 
   if (!options.name)
-    throw new AppError(AppErrorCode.ERR_COMMAND_NOT_FOUND, `ERR_COMMAND_NOT_FOUND`);
+    throw new AppError(AppErrorCode.ERR_COMMAND_NOT_FOUND, 'ERR_COMMAND_NOT_FOUND');
 
   if (!options.arguments)
-    throw new AppError(AppErrorCode.ERR_COMMAND_NOT_FOUND, `ERR_COMMAND_NOT_FOUND`);
+    throw new AppError(AppErrorCode.ERR_COMMAND_NOT_FOUND, 'ERR_COMMAND_NOT_FOUND');
 
   await Application.startCommand(config, options.name, options.arguments).catch(async (err: ExError) => {
-    Application.appLog.fatal('run-command', err, { error: Logger.errorMessage(err) });
+    Application.appLog.fatal('run-command', err, {error: Logger.errorMessage(err)});
     await Runtime.shutdown();
     process.exit(1);
   });
   process.exit(0);
-}
+};
 
-async function loadConfig(options: IStartupOptions) {
+const loadConfig = async (options: IStartupOptions) => {
   const configLoader = new ConfigLoader<IApplicationOptions>();
   const configPath = options.config;
   if (path.isAbsolute(configPath)) {
@@ -72,4 +72,4 @@ async function loadConfig(options: IStartupOptions) {
     await configLoader.load(path.resolve(process.cwd(), options.config));
   }
   return configLoader.getConfig();
-}
+};
